505dc9431f02d8f5f8c04b831ee4b5ef698d7d70
Jorge L <jorge.alfmur@gmail.com>
2018-05-17 21:13:54 -0500

    configuracion para crear servidor express y lo importamos en index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
+import app from './app';
 
 mongoose.connect('mongodb://localhost/social');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error en conexion:'));
-db.once('open', () => console.log('conexion exitosa'));
+db.once('open', () => {
+  console.log('conexion exitosa');
+  app.listen(3000, () => console.log('servidor corriendo en localhost:3000'));
+});
