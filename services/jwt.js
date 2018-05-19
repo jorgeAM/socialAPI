b568a2c5a08b8a1b4904022e8a564747335a30c6
Jorge L <jorge.alfmur@gmail.com>
2018-05-18 23:27:57 -0500

    servicio para generar token

---
+++ services/jwt.js
@@ -0,0 +1,27 @@
+import jwt from 'jwt-simple';
+import moment from 'moment';
+
+const secret = 'xxx';
+
+function codificar(user) {
+  const payload = {
+    sub: user._id,
+    name: user.name,
+    surname: user.surname,
+    email: user.email,
+    role: user.role,
+    image: user.image,
+    iat: moment().unix(),
+    exp: moment().add(30, 'days').unix,
+  };
+  return jwt.encode(payload, secret);
+}
+
+function decodificar(token) {
+  return jwt.decode(token, secret);
+}
+
+export {
+  codificar,
+  decodificar
+};
