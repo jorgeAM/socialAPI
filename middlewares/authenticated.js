64e8c54874180bafbaf4802dbb7e149ad3059699
Jorge L <jorge.alfmur@gmail.com>
2018-05-22 23:04:37 -0500

    middleware para comprobar autenticación

---
+++ middlewares/authenticated.js
@@ -0,0 +1,23 @@
+import { decodificar } from '../services/jwt';
+import moment from 'moment';
+
+export const isAuth = function (req, res, next) {
+  if (!req.headers.authorization) {
+    res.status(403).send({ message: 'La petición no tiene la cabecera de autenticación' });
+  }else {
+    let token = req.headers.authorization;
+    try {
+      const user = decodificar(token);
+      if (user.exp <= moment().unix()) {
+        res.status(401).send({ message: 'Token expirado' });
+      }else {
+        req.user = user;
+        console.log(req.user);
+      }
+    } catch (ex) {
+      res.status(404).send({ message: 'Token inválido' });
+    }
+
+    next();
+  }
+};
